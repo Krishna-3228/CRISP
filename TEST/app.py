798939bca1249a233b6f538444cea177254eff68
Krishna-3228 <tiksgupta20@gmail.com>
2025-02-15 21:50:53 +0530

    Added 'Drop All requests' button

--- TEST/app.py
+++ TEST/app.py
@@ -1,5 +1,5 @@
 import flask 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 import requests
 import sqlite3
 
@@ -16,6 +16,17 @@ def home():
     
     return render_template("home.html", entries=entries)
 
+def delete_all_records():
+    database = sqlite3.connect('./Captured_requests.db')
+    cursor = database.cursor()
+    cursor.execute('delete from all_requests')
+    database.commit()
+    database.close()
+
+@app.route('/dropallrequests', methods=['POST'])
+def dropallrequests():
+    delete_all_records()
+    return redirect(url_for("home"))  # Redirect back to home page
 
 if __name__ == "__main__":
     app.run(port=PORT)
